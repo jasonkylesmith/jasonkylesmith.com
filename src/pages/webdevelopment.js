90683e6fe8e02552def7a0ebf743e6c2a1a47128
Jason Smith <jasonkylesmith@gmail.com>
2022-01-13 12:30:02 -0500

    Added some sample text

--- src/pages/webdevelopment.js
+++ src/pages/webdevelopment.js
@@ -22,21 +22,56 @@ const WebDevelopment = props => {
                 HTML, CSS, and JavaScript with heavy use of React to build
                 elegant, smooth, and pleasing websites. Though my formal work in
                 web development is recent, I've had an off-and-on relationship
-                with different web technologies for close to 20 years. Recently,
-                I've really begun to lean into the Jamstack concept of using a
-                content management system and static-site generator to build
-                sites.
+                with different web technologies for close to 20 years. I work
+                for a software development company (
+                <a href="https://moonello.com" target="_new">
+                  Moonello
+                </a>
+                ) as a software engineer focusing on hybrid mobile application
+                projects.
               </p>
+              <h3>2022 Goals</h3>
+              <p>
+                In 2022, I'd definitely like to focus a bit on some personal
+                projects, things to do solo that will let me learn and
+                experience at my own pace.
+              </p>
+              <h4>Key Projects</h4>
+              <ul>
+                <li>jasonkylesmith.com Portfolio Website</li>
+                <li>Discord Bot</li>
+                <li>A Client Website</li>
+                <li>React Native Applicaation</li>
+              </ul>
             </div>
             <div className="row">
-              <div className="col-7"></div>
-              <div className="col-5">
-                <h4>Languages, Frameworks, and Libraries of Note</h4>
-                <FontAwesomeIcon icon={["fab", "html5"]} size="2x" />
-                <FontAwesomeIcon icon={["fab", "css3-alt"]} size="2x" />
-                <FontAwesomeIcon icon={["fab", "js-square"]} size="2x" />
-                <FontAwesomeIcon icon={["fab", "react"]} size="2x" />
-                <FontAwesomeIcon icon={["fab", "bootstrap"]} size="2x" />
+              <div className="col-12">
+                <h3>Languages, Frameworks, and Libraries of Note</h3>
+                <FontAwesomeIcon
+                  icon={["fab", "html5"]}
+                  size="2x"
+                  className="me-2"
+                />
+                <FontAwesomeIcon
+                  icon={["fab", "css3-alt"]}
+                  size="2x"
+                  className="me-2"
+                />
+                <FontAwesomeIcon
+                  icon={["fab", "js-square"]}
+                  size="2x"
+                  className="me-2"
+                />
+                <FontAwesomeIcon
+                  icon={["fab", "react"]}
+                  size="2x"
+                  className="me-2"
+                />
+                <FontAwesomeIcon
+                  icon={["fab", "bootstrap"]}
+                  size="2x"
+                  className="me-2"
+                />
               </div>
             </div>
           </div>
