dbd4eb30d33aa4105fed80f9b0b7416fab17d46c
Jason Smith <jasonkylesmith@gmail.com>
2022-05-22 00:26:23 -0400

    update style to client-gallery page

--- src/templates/client-gallery.js
+++ src/templates/client-gallery.js
@@ -104,51 +104,54 @@ const ClientGallery = props => {
     <Layout>
       <Seo title={name} />
       {enteredPassphrase === passphrase ? (
-        <div className="container-fluid p-0 mt-4">
-          <div className="row p-0">
-            <div className="col-sm-8 offset-sm-2">
-              {blocks.map(block => {
-                const { id } = block.sys.contentType.sys
+        <div className="row p-0">
+          <div className="col-sm-8 offset-sm-2">
+            {blocks.map(block => {
+              const { id } = block.sys.contentType.sys
 
-                if (id === "blockStory") {
-                  return (
-                    <div>
-                      <BlockStory block={block} key={block.id} />
-                    </div>
-                  )
-                }
+              if (id === "blockStory") {
+                return (
+                  <div>
+                    <BlockStory block={block} key={block.id} />
+                  </div>
+                )
+              }
 
-                if (id === "blockFeature") {
-                  return (
-                    <div>
-                      <BlockFeature block={block} key={block.id} />
-                    </div>
-                  )
-                }
+              if (id === "blockFeature") {
+                return (
+                  <div>
+                    <BlockFeature block={block} key={block.id} />
+                  </div>
+                )
+              }
 
-                if (id === "blockGallery") {
-                  return (
-                    <div>
-                      <BlockGallery block={block} key={block.id} />
-                    </div>
-                  )
-                }
-              })}
-            </div>
+              if (id === "blockGallery") {
+                return (
+                  <div>
+                    <BlockGallery block={block} key={block.id} />
+                  </div>
+                )
+              }
+            })}
           </div>
         </div>
       ) : (
-        <div className="row">
-          <div className="col d-flex justify-content-center align-content-center">
+        <div className="row p-0">
+          <div className="col-sm-8 offset-sm-2">
             <div
-              style={{ width: "50%", border: "2px solid pink" }}
+              style={{ border: "2px solid pink" }}
               className="d-flex flex-column justify-content-center"
             >
-              <h2>Please enter the passphrase given to you in your email.</h2>
+              <h2 className="m-0">Passphrase Required</h2>
+              <span>
+                Please enter the passphrase given to you in your email.
+              </span>
               <input
                 placeholder="********"
                 name="enteredPassphrase"
                 type="password"
+                value={enteredPassphrase}
+                onChange={e => setEnteredPassphrase(e.target.value)}
               />
             </div>
           </div>
